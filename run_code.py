412918dd72acb9967a1fab615484aeffa535df24
invisul <54588758+invisul@users.noreply.github.com>
2022-01-15 13:13:38 +0200

    did Q9

--- run_code.py
+++ run_code.py
@@ -7,7 +7,9 @@ import matplotlib.pyplot as plt
 from prepare_HW3 import prepare_data
 import numpy as np
 from models import compare_gradients ,test_lr
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split , cross_validate
+from sklearn.dummy import DummyRegressor
+from sklearn.linear_model import Ridge
 itai_id = 3
 or_id = 0
 
@@ -74,13 +76,89 @@ def Q5(train):
     y_val = train_subset_test['VirusScore'].values
     test_lr(X_train.values, y_train, X_val, y_val)
 
+def prepare_x_train_y_train(train):
+    train_subset, train_subset_test = train_test_split(train, train_size=0.8, random_state=itai_id + or_id)
+    X_train = train_subset.copy()
+    X_train.pop('VirusScore')
+    y_train = train_subset['VirusScore'].values
+    X_train = X_train
+    return X_train, y_train
+
+
+def Q6(train):
+    X_train, y_train = prepare_x_train_y_train(train)
+
+    dummy = DummyRegressor()
+    dummy.fit(X_train,y_train)
+    scores = cross_validate(dummy,X_train,y_train, scoring="neg_mean_squared_error",cv =5 ,return_train_score=True)
+    train_score = np.mean(scores['train_score'])
+    test_score = np.mean(scores['test_score'])
+    return train_score , test_score
+
+
+def get_train_valid_mse(model , X_train, y_train , split = 5):
+    scores = cross_validate(model, X_train, y_train, scoring="neg_mean_squared_error", cv=5, return_train_score=True)
+    train_score = np.mean(scores['train_score'])
+    test_score = np.mean(scores['test_score'])
+    return train_score, test_score
+
+def dummyRegressor(train):
+    X_train, y_train = prepare_x_train_y_train(train)
+
+    return get_train_valid_mse(DummyRegressor(),X_train,y_train)
+
+
+def Q7(train):
+
+    X_train, y_train = prepare_x_train_y_train(train)
+
+    dummy_train , dummy_val = dummyRegressor(train)
+    # scores = cross_validate(dummy, X_train, y_train, scoring="neg_mean_squared_error", cv=5, return_train_score=True)
+    # train_score = np.mean(scores['train_score'])
+    # test_score = np.mean(scores['test_score'])
+    alpha_sampling = np.logspace(-5,5,150)
+    eval_train, eval_val = [], []
+    for alpha in np.logspace(-5,5,150):
+        etraining, evalid = get_train_valid_mse(Ridge(alpha=alpha), X_train, y_train, 5)
+        eval_train.append(etraining)
+        eval_val.append(evalid)
+    plt.semilogx(np.logspace(-5,5,150), eval_train)
+    plt.semilogx(np.logspace(-5,5,150), eval_val)
+    plt.semilogx(np.logspace(-5,5,150), [dummy_val for _ in np.logspace(-5,5,150)])
+    plt.legend(["eval train", "eval val", "dummy"])
+    plt.xlabel("alpha")
+    plt.ylabel("error")
+    # plt.yscale('log')
+    plt.grid()
+    plt.show()
+    best_train_alpha = alpha_sampling[np.argmin(np.array(eval_train))]
+    print("best for train: error-", np.min(np.array(eval_train)), "alpha-", alpha_sampling[np.argmin(np.array(eval_train))])
+    print("best for validation: error-", np.min(np.array(eval_val)), "alpha-",
+          alpha_sampling[np.argmin(np.array(eval_val))])
+    return best_train_alpha
+
+
+def Q9(train):
+    X_train, y_train = prepare_x_train_y_train(train)
+    best_train_alpha = Q7(train)
+    new_reg = Ridge(best_train_alpha)
+    new_reg.fit(X_train,y_train)
+
+    tmp_arr = np.abs(np.array(new_reg.coef_))
+    indexes = tmp_arr.argsort()[::-1]
+    for i in indexes[:5]:
+        print(X_train.columns[i])
+    pass
+
+def Q10(train):
+
 
 if __name__ == '__main__':
     data = pd.read_csv("virus_labeled.csv")
     train_data, test_data = prepare_data(data)
     train_data, test_data = Q2(train_data, test_data)
-    Q4(train_data=train_data)
-    Q5(train_data)
-
-
-
+    # Q4(train_data=train_data)
+    # Q5(train_data)
+    # Q6(train_data)
+    # Q7(train_data)
+    Q9(train_data)
